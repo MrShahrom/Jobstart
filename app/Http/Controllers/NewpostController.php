6a5a3e2f3a6ffdafe1ca992107f50eb56a0a7eac
MrShahrom <shahromeo777@gmail.com>
2023-10-13 12:09:09 +0500

    initial commit

---
+++ app/Http/Controllers/NewpostController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NewpostController extends Controller
+{
+    public function newpost(){
+        return view('newpost');
+    }
+}
